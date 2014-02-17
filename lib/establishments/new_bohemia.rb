984c45814fcf53af045b22d2de353fc192ecd8f3
Dan Olson <olson_dan@yahoo.com>
2014-02-16 19:27:57 -0600

    Fix New Bohemia

--- lib/establishments/new_bohemia.rb
+++ lib/establishments/new_bohemia.rb
@@ -3,12 +3,18 @@ module BeerList
     class NewBohemia < Establishment
       URL     = 'http://www.newbohemiausa.com/bier/'
       ADDRESS = '233 E Hennepin Ave, Minneapolis, MN 55414'
-      CRUFT   = /Regional & International Drafts|\s\(Gluten free\) 5.8/
+      CRUFT   = /\s\(Gluten free\) 5.8/
+      LOCAL_DRAFTS     = 'Local Drafts'
+      REGIONAL_DRAFTS  = 'Regional & International Drafts'
+      BOTTLES_AND_CANS = 'Bottles & Cans'
+      SOCIAL_BOTTLES   = 'Social Bottles'
 
       def get_list
         @beers = base_list
         remove_cruft
         strip
+        fix_surly_abrasive
+        @beers
       end
 
       def url
@@ -22,7 +28,7 @@ module BeerList
       private
 
       def base_list
-        beers = split_on_newline raw_text
+        beers = split_on_newline raw_list
         match_before_slash(beers).compact
       end
 
@@ -34,8 +40,29 @@ module BeerList
         @beers = @beers.map { |beer| beer.gsub(/\A\W*|\W*\z/, '') }
       end
 
+      ###
+      # FIXME. This is gross. Should be replaced with some sort of
+      # canonical list and pattern matching.
+      def fix_surly_abrasive
+        abrasive = @beers.find { |b| b == 'Sulry Abrasive Double IPA 9' }
+        abrasive.sub! 'Sulry', 'Surly'
+        abrasive.sub! 'Double IPA 9', 'Ale'
+      end
+
       def raw_text
-        page.search('div.entry-content p')[7..9].map(&:text).flatten
+        @raw_text ||= page.search('div.entry-content p').map &:text
+      end
+
+      def raw_list
+        local_drafts     = raw_text[menu_section_index LOCAL_DRAFTS]
+        regional_drafts  = raw_text[menu_section_index REGIONAL_DRAFTS]
+        bottles_and_cans = raw_text[menu_section_index BOTTLES_AND_CANS]
+        social_bottles   = raw_text[menu_section_index SOCIAL_BOTTLES]
+        [local_drafts, regional_drafts, bottles_and_cans, social_bottles]
+      end
+
+      def menu_section_index(section)
+        raw_text.index(section) + 1
       end
 
       def split_on_newline(beers)
