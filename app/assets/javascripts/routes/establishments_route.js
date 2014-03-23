9924ce085b572508a7809e96290ec2a3378e298a
Dan Olson <olson_dan@yahoo.com>
2014-03-22 21:09:35 -0500

    Loading spinner

--- app/assets/javascripts/routes/establishments_route.js
+++ app/assets/javascripts/routes/establishments_route.js
@@ -1,5 +1,21 @@
 Beermapper.EstablishmentsRoute = Ember.Route.extend({
   model: function(){
     return this.store.find('establishment');
+  },
+
+  actions: {
+    loading: function(transition, originRoute){
+      var view = Ember.View.create({
+        container: this.container,
+        controller: this.controllerFor('application'),
+        templateName: 'global-loading',
+        elementId: 'loading'
+      }).append();
+
+      this.router.one('didTransition', function() {
+        view.destroy();
+      });
+      return false;
+    }
   }
 })
