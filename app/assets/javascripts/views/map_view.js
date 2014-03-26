59a031ebb63c4f8e2389111e83b9465d15fad2e4
Dan Olson <olson_dan@yahoo.com>
2014-03-25 19:50:40 -0500

    Fix map rendering issue

--- app/assets/javascripts/views/map_view.js
+++ app/assets/javascripts/views/map_view.js
@@ -1,7 +1,6 @@
 Beermapper.MapView = Ember.ContainerView.extend({
   didInsertElement: function(){
     var controller = this.get('controller');
-    var map        = controller.createMap(this.$()[0]);
     var infoWindow = new google.maps.InfoWindow({maxWidth:300});
 
     // Size the view's container so that the map can render
@@ -14,6 +13,7 @@ Beermapper.MapView = Ember.ContainerView.extend({
       }
     });
 
+    var map = controller.createMap(this.$()[0]);
     controller.set('infoWindow', infoWindow);
     controller.set('map', map);
     controller.set('mapView', this);
