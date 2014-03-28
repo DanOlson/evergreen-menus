15636359ed35db8e1dd6b5c54dab8c5555292f60
Dan Olson <olson_dan@yahoo.com>
2014-03-28 13:56:27 -0500

    Return errors in est suggestion json if any

--- app/serializers/v1/establishment_suggestion_serializer.rb
+++ app/serializers/v1/establishment_suggestion_serializer.rb
@@ -1,5 +1,9 @@
 module V1
   class EstablishmentSuggestionSerializer < AppSerializer
-    attributes :name, :beer_list_url
+    attributes :name, :beer_list_url, :errors
+
+    def include_errors?
+      object.errors.any?
+    end
   end
 end
