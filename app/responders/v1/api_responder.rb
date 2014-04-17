e62b2f50eb6c2e375a72136f1aa6d7ddfea9802a
Dan Olson <olson_dan@yahoo.com>
2014-04-16 20:28:59 -0500

    Clean up api responder

--- app/responders/v1/api_responder.rb
+++ app/responders/v1/api_responder.rb
@@ -1,17 +1,16 @@
 module V1
   class ApiResponder < ActionController::Responder
-    attr_reader :controller_with_namespace
+    attr_reader :controller_with_namespaces
 
     def initialize(controller, resources, options={})
-      @controller_with_namespace = controller.class.name.split('::')
-      add_serializer_options resources, options
+      @controller_with_namespaces = controller.class.name.split('::')
+      apply_serializer_options resources, options
       super
     end
 
     def resources
-      # Set location header with namespace
       if @resources.size == 1
-        @resources.unshift(*controller_with_namespace[0...-1].map { |n| n.downcase.to_sym })
+        @resources.unshift(*controller_with_namespaces[0...-1].map { |n| n.downcase.to_sym })
       else
         @resources
       end
@@ -19,20 +18,26 @@ module V1
 
     protected
 
-    def add_serializer_options(resources, options)
-      serializer_key = resources.last.respond_to?(:each) ? :each_serializer : :serializer
-      options[serializer_key] ||= serializer_for_api_version
+    def apply_serializer_options(resources, options)
+      key = resources.last.respond_to?(:each) ? :each_serializer : :serializer
+      options[key] ||= serializer_for_api_version
     end
 
     def serializer_for_api_version
-      resource_name = controller_with_namespace.last.sub('Controller', '').singularize
-      serializer_name = "#{resource_name}Serializer"
-      get_serializer_with_namespace serializer_name
+      get_serializer_with_namespace
     end
 
-    def get_serializer_with_namespace(serailizer_name)
-      namespace = controller_with_namespace[1]
-      [namespace, serailizer_name].join('::').safe_constantize
+    def resource_name
+      controller_with_namespaces.last.sub('Controller', '').singularize
+    end
+
+    def serializer_name
+      "#{resource_name}Serializer"
+    end
+
+    def get_serializer_with_namespace
+      version_namespace = controller_with_namespaces[1]
+      [version_namespace, serializer_name].join('::').safe_constantize
     end
   end
 end
