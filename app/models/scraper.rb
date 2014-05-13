37c54d75bba5ab006f177f6aa9f8ae74094d9b9d
Dan Olson <olson_dan@yahoo.com>
2014-05-12 20:07:57 -0500

    Only scrape active establishments

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -4,6 +4,8 @@ class Scraper < ActiveRecord::Base
 
   class << self
     def queued_for_update
+      joins(:establishment).
+      merge(Establishment.active).
       where arel_table[:scheduled_run_time].lt(Time.zone.now).and(arel_table[:last_ran_at].lt(23.hours.ago).or(arel_table[:last_ran_at].eq(nil)))
     end
   end
