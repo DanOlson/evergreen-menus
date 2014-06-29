589d75f13cd7d6c0e6397ec784b2fb59e13880d3
Dan Olson <olson_dan@yahoo.com>
2014-06-28 21:28:20 -0500

    Fix NewBohemia

--- lib/establishments/new_bohemia.rb
+++ lib/establishments/new_bohemia.rb
@@ -4,17 +4,17 @@ module BeerList
       URL     = 'http://www.newbohemiausa.com/bier/'
       ADDRESS = '233 E Hennepin Ave, Minneapolis, MN 55414'
       CRUFT   = /\s\(Gluten free\) 5.8/
-      LOCAL_DRAFTS     = 'Local Drafts'
-      REGIONAL_DRAFTS  = 'Regional & International Drafts'
-      BOTTLES_AND_CANS = 'Bottles & Cans'
-      SOCIAL_BOTTLES   = 'Social Bottles'
+      LOCAL_DRAFTS    = 'Local Drafts'
+      REGIONAL_DRAFTS = 'Regional Drafts'
+      TALLBOYS        = 'Tallboys'
+      SOCIAL_BOTTLES  = 'Social Bottles'
+      FLIGHTS         = 'Flights'
 
       def get_list
-        @beers = base_list
-        remove_cruft
-        strip
-        fix_surly_abrasive
-        @beers
+        beers = base_list
+        beers = remove_cruft beers
+        beers = strip beers
+        beers
       end
 
       def url
@@ -28,23 +28,16 @@ module BeerList
       private
 
       def base_list
-        beers = split_on_newline raw_list
+        beers = raw_list
         match_before_slash(beers).compact
       end
 
-      def remove_cruft
-        @beers = @beers.map { |beer| beer.sub CRUFT, '' }
+      def remove_cruft(beers)
+        beers.map { |beer| beer.sub CRUFT, '' }
       end
 
-      def strip
-        @beers = @beers.map { |beer| beer.gsub(/\A\W*|\W*\z/, '') }
-      end
-
-      ###
-      # FIXME. This is gross. Should be replaced with some sort of
-      # canonical list and pattern matching.
-      def fix_surly_abrasive
-        @beers = @beers.map { |b| b.sub 'Sulry', 'Surly' }
+      def strip(beers)
+        beers.map { |beer| beer.gsub(/\A\W*|\W*\z/, '') }
       end
 
       def raw_text
@@ -52,19 +45,35 @@ module BeerList
       end
 
       def raw_list
-        local_drafts     = raw_text[menu_section_index LOCAL_DRAFTS]
-        regional_drafts  = raw_text[menu_section_index REGIONAL_DRAFTS]
-        bottles_and_cans = raw_text[menu_section_index BOTTLES_AND_CANS]
-        social_bottles   = raw_text[menu_section_index SOCIAL_BOTTLES]
-        [local_drafts, regional_drafts, bottles_and_cans, social_bottles]
+        [local_drafts, regional_drafts, tallboys, social_bottles].reduce :+
+      end
+
+      def local_drafts
+        beers_between_sections LOCAL_DRAFTS, REGIONAL_DRAFTS
+      end
+
+      def regional_drafts
+        beers_between_sections REGIONAL_DRAFTS, FLIGHTS
+      end
+
+      def tallboys
+        beers_between_sections TALLBOYS, SOCIAL_BOTTLES
+      end
+
+      def social_bottles
+        split_on_newline raw_text[menu_section_index SOCIAL_BOTTLES]
+      end
+
+      def beers_between_sections(sec1, sec2)
+        raw_text[(menu_section_index(sec1))..(raw_text.index(sec2) - 1)]
       end
 
       def menu_section_index(section)
         raw_text.index(section) + 1
       end
 
-      def split_on_newline(beers)
-        beers.inject([]) { |ary, string| ary << string.split("\n") }.flatten
+      def split_on_newline(str)
+        str.split("\n")
       end
 
       def match_before_slash(beers)
