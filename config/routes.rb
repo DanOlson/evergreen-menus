6e8defa4fd6b43c1f24ab61e33a7b11355d9ad97
Dan Olson <olson_dan@yahoo.com>
2016-09-24 22:17:32 -0500

    Hosted menu very basic POC

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Beermapper::Application.routes.draw do
       resources :scrapers, only: :index
     end
   end
+
+  resources :menus, only: :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
