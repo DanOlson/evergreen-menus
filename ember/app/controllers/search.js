c84fab2c2f543408a3533ceb35b1562b4df5aa02
Dan Olson <olson_dan@yahoo.com>
2016-11-23 22:15:05 -0600

    Remove MapView and MapUtils

--- ember/app/controllers/search.js
+++ ember/app/controllers/search.js
@@ -1,11 +1,8 @@
 import Ember from 'ember';
-import MapUtils from '../mixins/map-utils';
 // import flashQueueController from '../controllers/flash-queue';
 
-var SearchController = Ember.Controller.extend({
+export default Ember.Controller.extend({
   applicationController: Ember.inject.controller('application'),
   query: Ember.computed.reads('applicationController.queryField'),
   queryParams: ['query']
 });
-
-export default SearchController;
