169c90a0ec824afd7001d5b296ec7ba131decc11
Dan Olson <olson_dan@yahoo.com>
2016-11-23 22:09:26 -0600

    Convert update-list-view to update-list-button component

--- ember/app/controllers/establishment.js
+++ ember/app/controllers/establishment.js
@@ -4,18 +4,12 @@ import flashQueueController from '../controllers/flash-queue';
 const EstablishmentController = Ember.Controller.extend({
   applicationController: Ember.inject.controller('application'),
   isAuthenticated: Ember.computed.reads('applicationController.isAuthenticated'),
-  updating: false,
-  flashQueueController: flashQueueController,
 
   actions: {
-    updateList: function(){
-      const listUpdate = this.store.createRecord('listUpdate', {
-        establishment: this.get('model')
-      });
-      this.set('updating', true);
-      listUpdate.save().then(() => {
+    updateList(establishment) {
+      const listUpdate = this.store.createRecord('listUpdate', { establishment });
+      return listUpdate.save().then(() => {
         flashQueueController.flash('notice', 'Beer list updated!');
-        this.set('updating', false);
       }).catch(() => {
         flashQueueController.flash('alert', 'Beer list was not updated!');
       });
