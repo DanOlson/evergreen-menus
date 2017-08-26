ad826caf5771b06d845f656eefb75cdae1e26ca9
Dan Olson <olson_dan@yahoo.com>
2017-08-26 09:55:08 -0500

    Update webpack config to uglify only on prod builds

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,57 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const jsOutputFile  = isProd ? 'javascripts/[name]-[hash].js' : 'javascripts/[name].js';
 const cssOutputFile = isProd ? 'stylesheets/[name]-[hash].css' : 'stylesheets/[name].css';
 
+const plugins = [
+  new CopyWebpackPlugin([
+    {
+      from: path.join(__dirname, 'app', 'assets', 'images'),
+      to: 'images/'
+    }
+  ]),
+  new ExtractTextPlugin({
+    filename: cssOutputFile
+  }),
+
+  function() {
+    // delete previous outputs
+    this.plugin("compile", function() {
+      let basepath = __dirname + "/public";
+      let paths = ["/javascripts", "/stylesheets"];
+
+      paths.forEach(path => {
+        const asset_path = basepath + path;
+
+        fs.readdir(asset_path, function(err, files) {
+          if (files === undefined) {
+            return;
+          }
+
+          files.forEach(file => {
+            fs.unlinkSync(asset_path + "/" + file);
+          })
+        });
+      })
+    });
+
+    // output the fingerprint
+    this.plugin("done", function(stats) {
+      let output = "ASSET_FINGERPRINT = \"" + stats.hash + "\""
+      fs.writeFileSync("config/initializers/fingerprint.rb", output, "utf8");
+    });
+  }
+]
+
+if (isProd) {
+  const definePlugin = new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(isProd ? 'production' : 'development')
+  });
+  const uglifyPlugin = new webpack.optimize.UglifyJsPlugin({
+    sourceMap: true
+  });
+  plugins.push(definePlugin);
+  plugins.push(uglifyPlugin);
+}
+
 module.exports = {
   context: __dirname + '/app/assets',
   entry: {
@@ -42,50 +93,5 @@ module.exports = {
     ]
   },
 
-  plugins: [
-    new CopyWebpackPlugin([
-      {
-        from: path.join(__dirname, 'app', 'assets', 'images'),
-        to: 'images/'
-      }
-    ]),
-    new ExtractTextPlugin({
-      filename: cssOutputFile
-    }),
-
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(isProd ? 'production' : 'development')
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true
-    }),
-
-    function() {
-      // delete previous outputs
-      this.plugin("compile", function() {
-        let basepath = __dirname + "/public";
-        let paths = ["/javascripts", "/stylesheets"];
-
-        paths.forEach(path => {
-          const asset_path = basepath + path;
-
-          fs.readdir(asset_path, function(err, files) {
-            if (files === undefined) {
-              return;
-            }
-
-            files.forEach(file => {
-              fs.unlinkSync(asset_path + "/" + file);
-            })
-          });
-        })
-      });
-
-      // output the fingerprint
-      this.plugin("done", function(stats) {
-        let output = "ASSET_FINGERPRINT = \"" + stats.hash + "\""
-        fs.writeFileSync("config/initializers/fingerprint.rb", output, "utf8");
-      });
-    }
-  ]
+  plugins: plugins
 };
