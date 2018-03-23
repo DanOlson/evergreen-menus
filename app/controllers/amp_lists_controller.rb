e41c5cb30c180a1fcce35359a3d74768d1490c9c
Dan Olson <olson_dan@yahoo.com>
2018-03-22 21:50:17 -0500

    Complete CORS implementation for amp-list requests

--- app/controllers/amp_lists_controller.rb
+++ app/controllers/amp_lists_controller.rb
@@ -6,30 +6,69 @@ class AmpListsController < ApplicationController
   after_action :set_cors_headers
 
   def show
-    list = List.find params[:id]
-    amp_list = AmpListSerializer.new(list).call
-    render json: amp_list, status: :ok
+    if !list
+      render json: { message: 'Not found' }, status: :not_found and return
+    end
+
+    if entitled?
+      amp_list = AmpListSerializer.new(list).call
+      render json: amp_list, status: :ok
+    else
+      render json: { message: 'Unauthorized' }, status: :unauthorized
+    end
   end
 
   private
 
+  def list
+    @list ||= List.find_by_id params[:id]
+  end
+
   def set_cors_headers
     response.set_header 'access-control-allow-headers', ACCESS_CONTROL_ALLOW_HEADERS
     response.set_header 'access-control-allow-credentials', 'true'
-    response.set_header 'amp-access-control-allow-source-origin', params['__amp_source_origin']
+    response.set_header 'amp-access-control-allow-source-origin', source_origin
     response.set_header 'access-control-allow-methods', ACCESS_CONTROL_ALLOW_METHODS
     response.set_header 'access-control-expose-headers', ACCESS_CONTROL_AMP_ALLOW_SOURCE_ORIGIN
     set_access_control_allow_origin
   end
 
-  ###
-  # TODO: Here we should check entitlements...
   def set_access_control_allow_origin
-    header_val = if request.headers['AMP-Same-Origin'] == 'true'
-      params['__amp_source_origin']
-    else
-      request.headers['Origin']
-    end
+    header_val = same_origin_request? ? source_origin : origin
     response.set_header 'access-control-allow-origin', header_val
+  end
+
+  def source_origin
+    params['__amp_source_origin']
+  end
+
+  def origin
+    request.headers['Origin']
+  end
+
+  def same_origin_request?
+    request.headers['AMP-Same-Origin'] == 'true'
+  end
+
+  def entitled?
+    entitled = source_origin_valid? && list.establishment.account.active?
+    same_origin_request? ? entitled : origin_allowed? && entitled
+  end
+
+  def origin_allowed?
+    allowed_origins = [
+      'cdn.ampproject.org',
+      'amp.cloudflare.com',
+      establishment_host
+    ].map { |host| '.*' + host }.join('|')
+    !!origin.match(/#{allowed_origins}/)
+  end
+
+  def establishment_host
+    URI(list.establishment.url).host
+  end
+
+  def source_origin_valid?
+    URI(source_origin).host == establishment_host
   end
 end
--- spec/requests/amp_lists_spec.rb
+++ spec/requests/amp_lists_spec.rb
@@ -1,11 +1,51 @@
 require 'spec_helper'
 
-describe 'getting data for amp-list' do
+describe 'CORS behavior around getting data for amp-list' do
   let(:establishment) do
     create :establishment, url: 'https://farbarmpls.com'
   end
   let(:list) do
     create :list, :with_items, item_count: 4, establishment: establishment
+  end
+
+  shared_examples 'AMP cache cross-origin request' do
+    let(:headers) do
+      {
+        'origin' => origin,
+        'Content-Type' => 'application/json'
+      }
+    end
+
+    before do
+      get "/amp/lists/#{list.id}.json?__amp_source_origin=https%3A%2F%2Ffarbarmpls.com", headers: headers
+    end
+
+    it 'sends the correct CORS response headers' do
+      expect(response).to have_http_status(:ok)
+      response_headers = response.headers
+      expect(response_headers['access-control-allow-headers']).to eq 'Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token'
+      expect(response_headers['access-control-allow-credentials']).to eq 'true'
+      expect(response_headers['access-control-allow-origin']).to eq origin
+      expect(response_headers['amp-access-control-allow-source-origin']).to eq 'https://farbarmpls.com'
+      expect(response_headers['access-control-allow-methods']).to eq 'GET, OPTIONS'
+      expect(response_headers['access-control-expose-headers']). to eq 'AMP-Access-Control-Allow-Source-Origin'
+    end
+
+    it 'returns the list items' do
+      parsed = JSON.parse(response.body)
+      expect(parsed['items'].size).to eq 4
+      expect(parsed['items']).to all satisfy { |item|
+        item.key?('name') && item.key?('description') && item.key?('price')
+      }
+    end
+  end
+
+  shared_examples 'an unauthorized request' do
+    it 'is unauthorized' do
+      expect(response).to have_http_status(:unauthorized)
+      expect(response.content_type).to eq 'application/json'
+      expect(JSON.parse(response.body)['message']).to eq 'Unauthorized'
+    end
   end
 
   describe 'GET to /amp/list/:id' do
@@ -17,59 +57,101 @@ describe 'getting data for amp-list' do
         }
       end
 
-      before do
-        get "/amp/lists/#{list.id}.json?__amp_source_origin=https%3A%2F%2Ffarbarmpls.com", headers: headers
+      context 'when the requested list exists' do
+        before do
+          get "/amp/lists/#{list.id}.json?__amp_source_origin=https%3A%2F%2Ffarbarmpls.com", headers: headers
+        end
+
+        it 'sends the correct CORS response headers' do
+          expect(response).to have_http_status(:ok)
+          response_headers = response.headers
+          expect(response_headers['access-control-allow-headers']).to eq 'Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token'
+          expect(response_headers['access-control-allow-credentials']).to eq 'true'
+          expect(response_headers['access-control-allow-origin']).to eq 'https://farbarmpls.com'
+          expect(response_headers['amp-access-control-allow-source-origin']).to eq 'https://farbarmpls.com'
+          expect(response_headers['access-control-allow-methods']).to eq 'GET, OPTIONS'
+          expect(response_headers['access-control-expose-headers']). to eq 'AMP-Access-Control-Allow-Source-Origin'
+        end
+
+        it 'returns the list items' do
+          parsed = JSON.parse(response.body)
+          expect(parsed['items'].size).to eq 4
+          expect(parsed['items']).to all satisfy { |item|
+            item.key?('name') && item.key?('description') && item.key?('price')
+          }
+        end
+
+        context 'when __amp_source_origin does not match establishment URL' do
+          before do
+            get "/amp/lists/#{list.id}.json?__amp_source_origin=https%3A%2F%2Fclosebarmpls.com", headers: headers
+          end
+
+          it_behaves_like 'an unauthorized request'
+        end
+
+        context "when the establishment's account is inactive" do
+          before do
+            account = establishment.account
+            account.update(active: false)
+            get "/amp/lists/#{list.id}.json?__amp_source_origin=https%3A%2F%2Ffarbarmpls.com", headers: headers
+          end
+
+          it_behaves_like 'an unauthorized request'
+        end
       end
 
-      it 'sends the correct CORS response headers' do
-        expect(response).to have_http_status(:ok)
-        response_headers = response.headers
-        expect(response_headers['access-control-allow-headers']).to eq 'Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token'
-        expect(response_headers['access-control-allow-credentials']).to eq 'true'
-        expect(response_headers['access-control-allow-origin']).to eq 'https://farbarmpls.com'
-        expect(response_headers['amp-access-control-allow-source-origin']).to eq 'https://farbarmpls.com'
-        expect(response_headers['access-control-allow-methods']).to eq 'GET, OPTIONS'
-        expect(response_headers['access-control-expose-headers']). to eq 'AMP-Access-Control-Allow-Source-Origin'
-      end
+      context 'when the requested list is not found' do
+        let(:list_id) { 509 }
 
-      it 'returns the list items' do
-        parsed = JSON.parse(response.body)
-        expect(parsed['items'].size).to eq 4
-        expect(parsed['items']).to all satisfy { |item|
-          item.key?('name') && item.key?('description') && item.key?('price')
-        }
+        before do
+          get "/amp/lists/#{list_id}.json?__amp_source_origin=https%3A%2F%2Ffarbarmpls.com", headers: headers
+        end
+
+        it 'returns 404' do
+          expect(response).to have_http_status :not_found
+          expect(response.content_type).to eq 'application/json'
+        end
       end
     end
 
-    context 'AMP cache cross-origin request' do
-      let(:headers) do
-        {
-          'origin' => 'https://farbarmpls-com.cdn.ampproject.org',
-          'Content-Type' => 'application/json'
-        }
+    context 'cross-origin request' do
+      context 'when the origin header is from https://farbarmpls-com.cdn.ampproject.org' do
+        let(:origin) { 'https://farbarmpls-com.cdn.ampproject.org' }
+
+        it_behaves_like 'AMP cache cross-origin request'
       end
 
-      before do
-        get "/amp/lists/#{list.id}.json?__amp_source_origin=https%3A%2F%2Ffarbarmpls.com", headers: headers
+      context 'when the origin header is from https://cdn.ampproject.org' do
+        let(:origin) { 'https://cdn.ampproject.org' }
+
+        it_behaves_like 'AMP cache cross-origin request'
       end
 
-      it 'sends the correct CORS response headers' do
-        expect(response).to have_http_status(:ok)
-        response_headers = response.headers
-        expect(response_headers['access-control-allow-headers']).to eq 'Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token'
-        expect(response_headers['access-control-allow-credentials']).to eq 'true'
-        expect(response_headers['access-control-allow-origin']).to eq 'https://farbarmpls-com.cdn.ampproject.org'
-        expect(response_headers['amp-access-control-allow-source-origin']).to eq 'https://farbarmpls.com'
-        expect(response_headers['access-control-allow-methods']).to eq 'GET, OPTIONS'
-        expect(response_headers['access-control-expose-headers']). to eq 'AMP-Access-Control-Allow-Source-Origin'
+      context 'when the origin header is from amp.cloudflare.com' do
+        let(:origin) { 'https://farbarmpls.com.amp.cloudflare.com' }
+
+        it_behaves_like 'AMP cache cross-origin request'
       end
 
-      it 'returns the list items' do
-        parsed = JSON.parse(response.body)
-        expect(parsed['items'].size).to eq 4
-        expect(parsed['items']).to all satisfy { |item|
-          item.key?('name') && item.key?('description') && item.key?('price')
-        }
+      context 'when the origin header is from the establishment URL' do
+        let(:origin) { 'https://farbarmpls.com' }
+
+        it_behaves_like 'AMP cache cross-origin request'
+      end
+
+      context 'when the origin header is anything else' do
+        let(:headers) do
+          {
+            'origin' => 'https://www.google.com',
+            'Content-Type' => 'application/json'
+          }
+        end
+
+        before do
+          get "/amp/lists/#{list.id}.json?__amp_source_origin=https%3A%2F%2Ffarbarmpls.com", headers: headers
+        end
+
+        it_behaves_like 'an unauthorized request'
       end
     end
   end
