8cb70e1d981f286e30645168b77cdf3e8d782ca5
Dan Olson <olson_dan@yahoo.com>
2018-05-27 18:02:12 -0500

    Add integration tests for role-based visibility of GMB integration functions

--- spec/features/connect_google_spec.rb
+++ spec/features/connect_google_spec.rb
@@ -10,7 +10,9 @@ feature 'Google OAuth2' do
     account_details = PageObjects::Admin::AccountDetails.new
     expect(account_details).to be_displayed
     expect(account_details).to have_connect_with_google_button
+    expect(account_details).to have_google_disabled_status
     expect(account_details).to_not have_disconnect_from_google_button
+    expect(account_details).to_not have_edit_google_link
   end
 
   scenario 'manager can disconnect their account from Google', :js, :admin do
@@ -26,7 +28,9 @@ feature 'Google OAuth2' do
     login manager
     account_details = PageObjects::Admin::AccountDetails.new
     expect(account_details).to be_displayed
+    expect(account_details).to have_edit_google_link
     expect(account_details).to have_disconnect_from_google_button
+    expect(account_details).to have_google_enabled_status
     expect(account_details).to_not have_connect_with_google_button
   end
 
@@ -35,5 +39,6 @@ feature 'Google OAuth2' do
     account_details = PageObjects::Admin::AccountDetails.new
     expect(account_details).to be_displayed
     expect(account_details).to_not have_connect_with_google_button
+    expect(account_details).to_not have_web_integrations
   end
 end
--- spec/support/page_objects/admin/account_details.rb
+++ spec/support/page_objects/admin/account_details.rb
@@ -6,6 +6,9 @@ module PageObjects
       class WebIntegrations < SitePrism::Section
         element :connect_with_google_button, '[data-test="connect-google-button"]'
         element :disconnect_from_google_button, '[data-test="disconnect-google-button"]'
+        element :edit_google_link, '[data-test="edit-google-link"]'
+        element :status_google_enabled, '[data-test="status-google-integration-enabled"]'
+        element :status_google_disabled, '[data-test="status-google-integration-disabled"]'
       end
 
       elements :establishments, '[data-test="establishment"]'
@@ -24,6 +27,18 @@ module PageObjects
 
       def has_disconnect_from_google_button?
         has_web_integrations? && web_integrations.has_disconnect_from_google_button?
+      end
+
+      def has_google_enabled_status?
+        has_web_integrations? && web_integrations.has_status_google_enabled?
+      end
+
+      def has_google_disabled_status?
+        has_web_integrations? && web_integrations.has_status_google_disabled?
+      end
+
+      def has_edit_google_link?
+        has_web_integrations? && web_integrations.has_edit_google_link?
       end
 
       def name
