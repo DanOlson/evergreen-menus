15960a2fb8a219a8c3934ebf7d0960e8e3082776
Dan Olson <olson_dan@yahoo.com>
2018-06-10 16:01:00 -0500

    Extend account decorator with Facebook support

--- app/decorators/account_decorator.rb
+++ app/decorators/account_decorator.rb
@@ -1,11 +1,14 @@
 class AccountDecorator < ApplicationDecorator
   delegate_all
 
-  attr_reader :google_my_business_service
+  attr_reader :google_my_business_service, :facebook_service
 
-  def initialize(account, google_my_business_service: nil)
+  def initialize(account,
+                 google_my_business_service: nil,
+                 facebook_service: nil)
     super(account)
     @google_my_business_service = google_my_business_service || default_gmb_service
+    @facebook_service = facebook_service || default_fb_service
   end
 
   def google_my_business_accounts
@@ -16,9 +19,17 @@ class AccountDecorator < ApplicationDecorator
     @google_my_business_locations ||= google_my_business_service.locations
   end
 
+  def facebook_pages
+    @facebook_pages ||= facebook_service.pages
+  end
+
   private
 
   def default_gmb_service
     GoogleMyBusiness::Service.new account: object
+  end
+
+  def default_fb_service
+    Facebook::Service.new account: object
   end
 end
--- spec/decorators/account_decorator_spec.rb
+++ spec/decorators/account_decorator_spec.rb
@@ -7,13 +7,19 @@ describe AccountDecorator do
   end
 
   it 'has a google_my_business_service' do
-    account = build_stubbed(:account)
+    account = build_stubbed :account
     instance = account.decorate
     expect(instance.google_my_business_service.account).to eq account
   end
 
+  it 'has a facebook_service' do
+    account = build_stubbed :account
+    instance = account.decorate
+    expect(instance.facebook_service.account).to eq account
+  end
+
   describe '#google_my_business_accounts' do
-    let(:account) { build_stubbed(:account) }
+    let(:account) { build_stubbed :account }
     let(:instance) do
       AccountDecorator.new(account, google_my_business_service: mock_gmb_service)
     end
@@ -24,6 +30,21 @@ describe AccountDecorator do
 
     it 'fetches the accounts from the google_my_business_service' do
       expect(instance.google_my_business_accounts).to eq gmb_accounts
+    end
+  end
+
+  describe '#facebook_pages' do
+    let(:account) { build_stubbed :account }
+    let(:instance) do
+      AccountDecorator.new(account, facebook_service: mock_fb_service)
+    end
+    let(:mock_fb_service) do
+      double(Facebook::Service, pages: fb_pages)
+    end
+    let(:fb_pages) { [Facebook::Page.new] }
+
+    it 'fetches the pages from the facebook_service' do
+      expect(instance.facebook_pages).to eq fb_pages
     end
   end
 end
