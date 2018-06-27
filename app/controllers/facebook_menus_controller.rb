e3ac1e04242bbc201ab73dd928161dd5f383b7bd
Dan Olson <olson_dan@yahoo.com>
2018-06-26 21:16:09 -0500

    Delete X-FRAME-OPTIONS on Facebook menu response

--- app/controllers/facebook_menus_controller.rb
+++ app/controllers/facebook_menus_controller.rb
@@ -10,7 +10,7 @@ class FacebookMenusController < ApplicationController
       render :not_found, layout: false, status: :payment_required
     else
       @online_menu = establishment.online_menu
-      response.headers['X-FRAME-OPTIONS'] = 'ALLOW-FROM https://www.facebook.com'
+      response.headers.delete 'X-FRAME-OPTIONS'
       render :show, layout: 'facebook_menu_tab'
     end
   end
