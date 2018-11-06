1dff0871b745de9304d53215cc91d4c917c05543
Dan Olson <olson_dan@yahoo.com>
2018-11-05 21:43:16 -0600

    Focus onboarding email CTA to create list only

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,5 +1,6 @@
 class WelcomeMailer < ApplicationMailer
   def welcome_email(email_address)
+    attachments.inline['create-list.jpg'] = File.read(Rails.root.join('public', 'images', 'create-list.jpg'))
     make_bootstrap_mail({
       to: email_address,
       subject: 'Welcome to Evergreen Menus!'
